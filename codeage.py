571779e9ab9eab30d8c0840e057be07d129c51a8
Fire Crow <fire@firecrow.com>
2015-01-02 22:47:01 -0500

    first working query to git

--- codeage.py
+++ codeage.py
@@ -1,37 +1,67 @@
+#!/usr/bin/env python
+import datetime, subprocess, re
+
+def get_datetime_avg(filepath, relative_date):
+    handle = subprocess.Popen(['git', 'blame', filepath], stdout=subprocess.PIPE)
+    lines = handle.stdout.read().split('\n')
+    extracted = []
+    for l in lines:
+        me = re.search('(\d{4}-\d{2}-\d{2})', l)
+        if me:
+            extracted.append(me.group(0))
+
+    relatives = [
+            (relative_date - datetime.datetime.strptime(x, '%Y-%m-%d')).days 
+                for x in extracted]
+
+    avg_days = sum(relatives) / len(relatives)
+    avg_age = relative_date - datetime.timedelta(days=avg_days) 
+    return avg_age
+
+
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(
-        description="Bucket lines or percentage of file in git blame\nto show code age average or number since a given date")
 
-    parser.add_argument('--after','-t', type=str, nargs='?',
-        help="date (e.g. 2012-01-21) show only counts for lines altered after this date")
+    def process_args():
+        parser = argparse.ArgumentParser(
+            description="Bucket lines or percentage of file in git blame\nto show code age average or number since a given date")
 
-    parser.add_argument('--depth','-d', type=int, nargs='?',
-        help="n aggregate values to the n folder level")
-    parser.add_argument('--files','-f', action='store_true',
-        help="show numbers for all files (DEFAULT)")
+        parser.add_argument('--after','-t', type=str, nargs='?',
+            help="date (e.g. 2012-01-21) show only counts for lines altered after this date")
 
-    parser.add_argument('--lines','-l', action='store_true',
-        help="show line numbers, meaingless without --after")
-    parser.add_argument('--average','-a', type=bool,
-        help="show the average date for the file if --files or files in folder if --folders (DEFAULT)")
+        parser.add_argument('--depth','-d', type=int, nargs='?',
+            help="n aggregate values to the n folder level")
 
-    args_obj = parser.parse_args()
-    # lack of dict comprehensions in python 2.6
-    args = {}
-    for k,v in args_obj._get_kwargs():
-        args[k] = v
+        parser.add_argument('--files','-f', nargs='+',
+            help="files and directories to gather data from")
 
-    # set defaults
-    if args['depth']:
-        args['type'] = 'folder'
-    else:
-        args['type'] = 'files'
+        parser.add_argument('--lines','-l', action='store_true',
+            help="show line numbers, meaingless without --after")
+        parser.add_argument('--average','-a', type=bool,
+            help="show the average date for the file if --files or files in folder if --folders (DEFAULT)")
 
-    if not args['lines']:
-        args['average'] = True
+        args_obj = parser.parse_args()
+        # lack of dict comprehensions in python 2.6
+        _args = {}
+        for k,v in args_obj._get_kwargs():
+            _args[k] = v
+
+        # set defaults
+        if _args['depth']:
+            _args['type'] = 'folder'
+        else:
+            _args['type'] = 'files'
+
+        if not _args['lines']:
+            _args['average'] = True
+        return _args
+
+    args = process_args()
         
-    print args
+    now = datetime.datetime.now()
+    if args['type'] == 'files':
+        for f in args['files']:
+            get_datetime_avg(f, now)
 
 
 
